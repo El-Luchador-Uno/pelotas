2b643df12ddbe59fcd703c423a89f98dfd3b1996
tyscluff <tyscluff@gmail.com>
2025-06-21 16:23:37 -0600

    Idk for sure

--- controller/connect_controller.py
+++ controller/connect_controller.py
@@ -1,6 +1,6 @@
 from evdev import InputDevice, ecodes, list_devices
-from pin_management import servo_pin, standard_pin
-from constants import A_BUTTON_CODE, B_BUTTON_CODE, Y_BUTTON_CODE
+from pin_management import servo_pin
+from constants import A_BUTTON_CODE, B_BUTTON_CODE, Y_BUTTON_CODE, INTAKE_SERVO, X_BUTTON_CODE
 from controller.handle_joystick_move import Direction, handle_move, get_degrees_of_direction
 
 def connect_controller():
@@ -25,9 +25,11 @@ def connect_controller():
     for event in controller.read_loop():
         if event.type == ecodes.EV_KEY or event.type == ecodes.EV_ABS:
             if event.code == A_BUTTON_CODE and event.value == 1:
-                standard_pin.control_pin(17, 1.0)
+                servo_pin.control_servo(INTAKE_SERVO, -1)
             elif event.code == B_BUTTON_CODE and event.value == 1:
-                standard_pin.control_pin(17, 0)
+                servo_pin.control_servo(INTAKE_SERVO, 0)
+            elif event.code == X_BUTTON_CODE and event.value == 1:
+                servo_pin.control_servo(INTAKE_SERVO, 1)
             elif event.code == Y_BUTTON_CODE and event.value == 1:
                 exit(0)
             
@@ -40,16 +42,16 @@ def connect_controller():
             angle = get_degrees_of_direction(x, y)
 
             if (angle >= 315 or angle < 45):
-                DIRECTION = Direction.LEFT
+                DIRECTION = Direction.RIGHT
             # Down: 45° to 135°
             elif 45 <= angle < 135:
-                DIRECTION = Direction.DOWN
+                DIRECTION = Direction.UP
             # Right: 135° to 225°
             elif 135 <= angle < 225:
-                DIRECTION = Direction.RIGHT
+                DIRECTION = Direction.LEFT
             # Up: 225° to 315°
             elif 225 <= angle < 315:
-                DIRECTION = Direction.UP
+                DIRECTION = Direction.DOWN
             else:
                 DIRECTION = Direction.STOP
 
--- pin_management/servo_pin.py
+++ pin_management/servo_pin.py
@@ -2,15 +2,14 @@ from gpiozero import Servo
 
 servo_objects = {}
 
-def control_servo(pin: int, state: bool):
+def control_servo(pin: int, value: float):
     if pin not in servo_objects:
         servo_objects[pin] = Servo(pin)
     
     servo = servo_objects[pin]
 
-    if state:
-        print("on")
-        servo.value = -1
-    else:
-        print("off")
+    if value == 0:
         servo.detach()
+        return
+
+    servo.value = value
