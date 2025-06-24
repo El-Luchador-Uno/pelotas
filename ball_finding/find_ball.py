35c1a15becf573e7156b382c6ad1cda584eac22c
tyscluff <tyscluff@gmail.com>
2025-06-24 08:34:37 -0600

    Rate limit the fps and flush frames to ease compute load and get recent frame

--- ball_finding/find_ball.py
+++ ball_finding/find_ball.py
@@ -1,19 +1,31 @@
 import cv2
 from ultralytics import YOLO
 import os
+import time
 
 def find_ball():
     model_path = os.path.join(os.path.dirname(__file__), "best.torchscript")
-    model = YOLO(model_path)
+    model = YOLO(model_path, task="detect")
     
-    cap = cv2.VideoCapture(0) 
+    cap = cv2.VideoCapture(0)
+
+    desired_fps = 0.25 
+    frame_interval = 1.0 / desired_fps 
     
     while True:
+        start_time = time.time()
+        
+        num_of_frames_to_flush = 5
+
+        for _ in range(num_of_frames_to_flush):
+            cap.read()
+
         ret, frame = cap.read()
         if not ret:
             break
 
-        print(frame.shape)
+        # If needed we can resize the frame to ease the compute load, but the model performance is significantly worse
+        # frame = cv2.resize(frame, (frame.shape[1] // 2, frame.shape[0] // 2)) 
             
         results = model.predict(source=frame, imgsz=640, conf=0.5)
 
@@ -24,16 +36,20 @@ def find_ball():
                 conf = float(box.conf[0])
                 print(f"Ball detected at ({x1}, {y1}) to ({x2}, {y2}) with confidence: {conf:.2f}")
                 
-                # Draw on frame for processing (but don't display)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 cv2.putText(frame, f"Bouncy Ball: {conf:.2f}", (x1, y1 - 10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-                
-                cv2.circle(frame, (960, 1070), 10, (255, 0, 0), 2)
+
+                # TODO: need to programmtically find the bottom center of the frame
+                cv2.circle(frame, (960, 1070), 10, (255, 0, 0), 2) 
         
         # Check for keyboard interrupt to exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+        elapsed = time.time() - start_time
+        if elapsed < frame_interval:
+            time.sleep(frame_interval - elapsed)
     
     # Clean up
     cap.release()
